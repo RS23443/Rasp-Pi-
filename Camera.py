a3cd99e4a7b7e2f7d7aebf4a4c5980f9dbb4e5ac
RS23443 <23443.rs@gmail.com>
2024-06-17 12:47:48 -0500

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -1,6 +1,12 @@
 from picamera import PiCamera
 from time import sleep
 from libcamera import LibCamera
+from gpiozero import Motor
+
+#def variable
+lback = motor(#pin1,pin2)
+  rba
+  
 
 camera.start_preview()
 sleep(5)
@@ -11,5 +17,19 @@ camera.resolution (1080,1080)
 camera.start_preview()
 #preview: open stream 
 sleep (2)
-camera.capture('jpeg.png')
+camera.capture('uno.jpeg')
 
+sleep (2)
+
+sleep(2)
+camera.capture('dos.jpeg')
+camera.stop_preview()
+
+f = open("uno.jpeg")
+print(f)
+g= open("dos.jpeg")
+
+if f == g
+  lback(0.25)
+  
+  
