e8e55b16d18985ee50e9f1f76d52e524b6c43667
RS23443 <23443.rs@gmail.com>
2024-06-19 14:37:16 -0500

    yummy

--- RobotControl.py
+++ RobotControl.py
@@ -1,6 +1,7 @@
 import gpiozero
 from time import sleep, time
 import RPi.GPIO as GPI1 # for color sensor program
+
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # GPIO values for the signal and photoreceptor pins - color sensor - new gpio number since they interfere with the motor numbers
@@ -86,7 +87,6 @@ def l_loop():
     duration = time() - start
     blue = lNUM_CYCLES / duration
     
-
     GPI1.output(ls2,GPI1.HIGH)
     GPI1.output(ls3,GPI1.HIGH)
     sleep(0.3)
@@ -100,11 +100,11 @@ def left_color_checker():
   left_setup()
   l_loop()
   if red > 12000 and blue < 7000 and green <7000:
-    return("1")
+    return("1.0")
   elif blue >12000 and green < 7000 and red < 7000:
-    return("2")
+    return("2.0")
   elif blue >10000 and green > 10000 and red > 10000:
-    return("0")
+    return("0.0")
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------------   
 # values are <7000 and >12000 -> for the RGB value you want use if and elif
@@ -141,7 +141,6 @@ def r_loop():
     duration = time() - start
     blue = rNUM_CYCLES / duration
     
-
     GPI1.output(rs2,GPI1.HIGH)
     GPI1.output(rs3,GPI1.HIGH)
     sleep(0.3)
@@ -155,11 +154,11 @@ def right_color_checker():
   right_setup()
   r_loop()
   if red > 12000 and blue < 7000 and green <7000:
-    return("red")
+    return("1.0")
   elif blue >12000 and green < 7000 and red < 7000:
-    return("blue")
+    return("2.0")
   elif blue >10000 and green > 10000 and red > 10000:
-    return("n/a")
+    return("0.0")
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -169,19 +168,48 @@ def straight_line_following():
    left_co = int(left_color_checker())
    right_co = int(right_color_checker())
   
-   if left_co == right_co and left_co == blue:
+   if left_co == right_co and left_co == 2:
       drivetrain.forward()
-   elif left_co == right_co and left_co == red:
+   elif left_co == right_co and left_co == 1:
       drivetrain.stop()
    elif left_co != right_co and (left_co == n/a or right_co == n/a):   
-     if left_co == n/a:
+     if left_co == 0:
        drivetrain.right()
-     if right_co == n/a:
+     elif right_co == 0:
        drivetrain.left()
-     elif left_co= n/a and right_co == n/a
+     elif left_co= 0 and right_co == 0
        drivetrain.stop()
 # follwoig taped path 
 
-      
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# distance sesnor code 
+
+f_dis_sensor = gpiozero.DistanceSensor(#Gpiozero number, second number, max_distance = 3, threshold_distance = 0.2) front distance sensor
+l_dis_sensor = gpiozero.DistanceSensor(#Gpiozero number, second number, max_distance = 3, threshold_distance = 0.2) left distance sensor
+r_dis_sensor = gpiozero.DistanceSensor(#Gpiozero number, second number, max_distance = 3, threshold_distance = 0.2) right distance sensor
+b_dis_sensor = gpiozero.DistanceSensor(#Gpiozero number, second number, max_distance = 3, threshold_distance = 0.2) back distance sensor
+
+def distance_checker(): 
+  while f_dis_sensor.in_range:
+    if l_dis_sesnor.in_range:
+      drivertrain.right
+    elif r_dis_sensor.in_range
+      drivertrain.left
+    elif l_dis_sensor.in_range && r_dis_sensor.in_range:
+      while l_dis_sensor.in_range && r_dis_sensor.in_range:
+        drivetrain.backward
+        b_dis_sensor.when_in_range = drivertain.stop 
+    elif
+      drivetrain.stop
+  
+def move_until_w/o_color:
+  while f_dis_sensor.out_of_range:  
+    drivetrain.forward
+  
+  #dis_sensor.when_in_range = drivetrain.stop
+  #dis_sensor.when_out_of_range = drivetrain.forward
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
   
   
